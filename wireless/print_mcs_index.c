041c684a5c6a4012b5f493adafa1f54f8ebe4bf2
lucasduffey <lucasduffey@users.noreply.github.com>
2017-09-02 11:02:26 -0500

    Create print_mcs_index.c

---
+++ wireless/print_mcs_index.c
@@ -0,0 +1,36 @@
+// iw/util.c - underflow
+//  * Not important
+
+static void print_mcs_index(const __u8 *mcs)
+{
+	unsigned int mcs_bit, prev_bit = -2, prev_cont = 0;
+
+	for (mcs_bit = 0; mcs_bit <= 76; mcs_bit++) {
+		unsigned int mcs_octet = mcs_bit/8;
+		unsigned int MCS_RATE_BIT = 1 << mcs_bit % 8;
+		bool mcs_rate_idx_set;
+
+		mcs_rate_idx_set = !!(mcs[mcs_octet] & MCS_RATE_BIT);
+
+		if (!mcs_rate_idx_set)
+			continue;
+
+		if (prev_bit != mcs_bit - 1) { // XXX: underflow by default
+			if (prev_bit != -2)
+				printf("%d, ", prev_bit);
+			else
+				printf(" ");
+			printf("%d", mcs_bit);
+			prev_cont = 0;
+		} else if (!prev_cont) {
+			printf("-");
+			prev_cont = 1;
+		}
+
+		prev_bit = mcs_bit;
+	}
+
+	if (prev_cont)
+		printf("%d", prev_bit);
+	printf("\n");
+}
